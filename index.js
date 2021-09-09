9c5a8089e46189df2d983e7ed59f4048cb4fdfe9
Lpslopes <lamartinelps@gmail.com>
2021-09-08 21:11:21 -0300

    Usando o Nodemon

--- index.js
+++ index.js
@@ -1,6 +1,19 @@
 const express = require('express');// importando o express
 const app = express(); // Iniciando o express
 
+
+
+app.get("/",function(req,res){
+    res.send("Bem Vindo Lamartine");
+
+});
+
+app.get("/blog",function(req,res){
+    res.send("Bem vindo ao blog!!");
+})
+
+
+
 app.listen(4000, function(erro){
     if(erro){
         console.log("Ocorreu um erro");
